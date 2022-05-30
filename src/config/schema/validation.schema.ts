8d0adfd2db04ac48243bfca4752308ac9af6196b
Nghia Ngo Quang <nqnghia285@gmail.com>
2022-05-30 17:30:19 +0700

    make it better

--- src/config/schema/validation.schema.ts
+++ src/config/schema/validation.schema.ts
@@ -13,17 +13,51 @@ export const validationSchema = Joi.object().keys({
    HOST: Joi.string()
       .valid('0.0.0.0', 'localhost', '127.0.0.1')
       .default('0.0.0.0'),
-   PORT: Joi.number().port().default(80),
+   PORT: Joi.number()
+      .port()
+      .default(80),
    ORIGIN: Joi.alternatives()
-      .try(Joi.string(), Joi.array().items(Joi.string())) // Issue: Can't check with uri()
+      .try(
+         Joi.string(),
+         Joi.array()
+            .items(Joi.string())
+      ) // Issue: Can't check with uri()
       .required(),
-   URL: Joi.string().uri().required(),
-   GRAPHQL_PATH: Joi.string().required(),
-   GRAPHQL_SCHEMA_PATH: Joi.string().required(),
-   DATABASE_URL: Joi.string().uri().required(),
-   JWT_KEY: Joi.string().required(),
-   TOKEN_NAME: Joi.string().required(),
-   AUTHOR: Joi.string().required(),
-   REDIS_SERVER_NAME: Joi.string().required(),
-   REDIS_SERVER_PORT: Joi.number().port().default(6379),
+   URL: Joi.string()
+      .uri()
+      .required(),
+   GRAPHQL_PATH: Joi.string()
+      .required(),
+   GRAPHQL_SCHEMA_PATH: Joi.string()
+      .required(),
+   DATABASE_URL: Joi.string()
+      .uri()
+      .required(),
+   JWT_KEY: Joi.string()
+      .required(),
+   TOKEN_NAME: Joi.string()
+      .required(),
+   AUTHOR: Joi.string()
+      .default('user'),
+   REDIS_SERVER_NAME: Joi.string()
+      .default('localhost'),
+   REDIS_SERVER_PORT: Joi.number()
+      .port()
+      .default(6379),
+   email: Joi.string()
+      .email()
+      .required(),
+   password: Joi.string()
+      .max(16)
+      .required(),
+   fullname: Joi.string()
+      .max(60)
+      .required(),
+   phone: Joi.string()
+      .min(12)
+      .max(13)
+      .required(),
+   role: Joi.string()
+      .valid('ADMIN', 'STAFF')
+      .default('ADMIN'),
 })
