a60d29e06ce31a6fae7ac61b3b9b517c5772f850
Nghia Ngo Quang <nqnghia285@gmail.com>
2022-07-02 15:38:32 +0700

    new update

--- src/config/schema/validation.schema.ts
+++ src/config/schema/validation.schema.ts
@@ -13,52 +13,22 @@ export const validationSchema = Joi.object().keys({
    HOST: Joi.string()
       .valid('0.0.0.0', 'localhost', '127.0.0.1')
       .default('0.0.0.0'),
-   PORT: Joi.number()
-      .port()
-      .default(80),
-   ORIGIN: Joi.alternatives()
-      .try(
-         Joi.string(),
-         Joi.array()
-            .items(Joi.string())
-      ) // Issue: Can't check with uri()
-      .required(),
-   URL: Joi.string()
-      .uri()
-      .required(),
-   GRAPHQL_PATH: Joi.string()
-      .default('/graphql'),
-   GRAPHQL_SCHEMA_PATH: Joi.string()
-      .default('src/graphql/schema.gql')
-      .required(),
-   DATABASE_URL: Joi.string()
-      .uri()
-      .required(),
-   JWT_KEY: Joi.string()
-      .required(),
-   TOKEN_NAME: Joi.string()
-      .default('Authorization'),
-   AUTHOR: Joi.string()
-      .default('user'),
-   REDIS_SERVER_NAME: Joi.string()
-      .default('localhost'),
-   REDIS_SERVER_PORT: Joi.number()
-      .port()
-      .default(6379),
-   email: Joi.string()
-      .email()
-      .required(),
-   password: Joi.string()
-      .max(16)
-      .required(),
-   fullname: Joi.string()
-      .max(60)
-      .required(),
-   phone: Joi.string()
-      .min(12)
-      .max(13)
-      .required(),
-   role: Joi.string()
-      .valid('ADMIN', 'STAFF')
-      .default('ADMIN'),
+   PORT: Joi.number().port().default(5000),
+   ORIGIN: Joi.string().default(
+      `["http://localhost:3000","http://127.0.0.1:3000","http://localhost:5000","http://127.0.0.1:5000"]`
+   ),
+   URL: Joi.string().uri().default('http://localhost'),
+   GRAPHQL_PATH: Joi.string().default('/graphql'),
+   GRAPHQL_SCHEMA_PATH: Joi.string().default('src/graphql/schema.gql'),
+   DATABASE_URL: Joi.string().uri().required(),
+   JWT_KEY: Joi.string().required(),
+   TOKEN_NAME: Joi.string().default('Authorization'),
+   AUTHOR: Joi.string().default('user'),
+   REDIS_SERVER_NAME: Joi.string().default('localhost'),
+   REDIS_SERVER_PORT: Joi.number().port().default(6379),
+   email: Joi.string().email().required(),
+   password: Joi.string().max(16).required(),
+   fullname: Joi.string().max(60).required(),
+   phone: Joi.string().min(12).max(13).required(),
+   role: Joi.string().valid('ADMIN', 'STAFF').default('ADMIN'),
 })
